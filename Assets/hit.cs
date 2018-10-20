e42b4e9de56e9394240370d1bfe549587b690f0f
peggy87222 <peggy87222@gmail.com>
2018-10-20 11:00:29 +0800

    add yumefirst

---
+++ Assets/hit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hit : MonoBehaviour {
+    public Animator anim;
+    private Collider col;
+    // Use this for initialization
+    
+	void Start () {
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider col) {
+        if (col.gameObject.tag == "robot")
+        {
+            anim.SetBool("hit",true);
+            anim.Play("die");
+        }
+    }
+}
---
+++ Assets/robot.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class robot : MonoBehaviour {
+	public int hp;
+	[Range(0,100)]
+	public int hpMax = 100;
+
+	public ParticleSystem bloodEFX;
+	//public Text hpText;
+	public Slider hpSlider;
+
+	// Use this for initialization
+	void Start () {
+		hp =  hpMax;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hp = Mathf.Clamp (hp, 0, 100);
+		hpSlider.value = (float)hp / (float)hpMax;
+
+		if (hp <= 0) {
+			//Die ();
+		}
+		
+	}
+
+	public void PlayBloodEFX(){
+		bloodEFX.Play ();
+
+	}
+	/*void OnTriggerEnter(Collider col){
+		Vector3 lookDir= Camera.main.transform.position - transform.position;
+		bloodEFX.transform.rotation = Quaternion.LookRotation (lookDir);
+		PlayBloodEFX ();
+		hp = hp - 10;
+}
+	void Die(){
+		Destroy (gameObject);
+	}*/
+}
